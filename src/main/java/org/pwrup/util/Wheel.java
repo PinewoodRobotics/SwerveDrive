48194ba98d14b1b315ef840722a92991c0ae09f4
brigerodev <golden.notch.creeper@gmail.com>
2025-02-15 18:40:29 -0800

    add simple wheel frame size

--- src/main/java/org/pwrup/util/Wheel.java
+++ src/main/java/org/pwrup/util/Wheel.java
@@ -21,4 +21,25 @@ public class Wheel {
     this.mover = mover;
     this.normal = new Vec2(position.getY(), -position.getX());
   }
+
+  public enum WheelPosition {
+    FRONT_LEFT(-1, 1),
+    FRONT_RIGHT(-1, -1),
+    REAR_LEFT(1, 1),
+    REAR_RIGHT(1, -1);
+
+    private final double x, y;
+    private WheelPosition(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public Vec2 getPosition(double scale) {
+      return new Vec2(this.x * scale, this.y * scale);
+    }
+  }
+
+  public static Wheel fromFrameSize(float squareSideSize, WheelPosition pos, WheelMover mover) {
+    return new Wheel(pos.getPosition(squareSideSize), mover);
+  }
 }
